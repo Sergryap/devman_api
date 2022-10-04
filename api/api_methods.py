ab5aa79914fe517c497879c3465f6fd6ccd3c36c
sergryap <rs1180@mail.ru>
2022-10-04 19:53:57 +0500

    Implementing a check waiting method in an infinite Loop

--- api/api_methods.py
+++ api/api_methods.py
@@ -1,23 +1,32 @@
 import requests
 import json
+from pprint import pprint
 
 from api.oauth import TOKEN_DEV, TOKEN_TG
 
 
 class ApiMethod:
-
     URL_USER = "https://dvmn.org/api/user_reviews/"
     URL_LONG = "https://dvmn.org/api/long_polling/"
     URL_TG = "https://api.telegram.org/bot"
-    TOKEN_DEV = TOKEN_DEV
     TOKEN_TG = TOKEN_TG
+    HEADERS = {'Authorization': f'Token {TOKEN_DEV}'}
 
     def __init__(self, chat_id: int):
         self.chat_id = chat_id
 
-    def long_polling(self):
-        """Метод api devman"""
-        response = requests.get(self.URL_USER, headers={'Authorization': f'Token {self.TOKEN_DEV}'}).json()
+    def long_polling(self, timestamp=None):
+        """Метод long_polling api devman. Метод разовой проверки"""
+        url = self.URL_LONG
+        if timestamp:
+            url += f"?timestamp={round(timestamp, 0)}"
+        return requests.get(self.URL_LONG, headers=self.HEADERS).json()
+
+    def long_polling_timeout(self):
+        """Метод получения результата проверки в режиме ожидания в цикле"""
+        response = self.long_polling()
+        while response['status'] == 'timeout':
+            response = self.long_polling(timestamp=response['timestamp_to_request'])
         return response
 
     def send_message(self, msg: str):
@@ -25,8 +34,15 @@ class ApiMethod:
         requests.get(rf"{self.URL_TG}{self.TOKEN_TG}/sendmessage?chat_id={self.chat_id}&text={msg}")
 
 
+def main():
+    """Функция получения результата проверок в ожидании в бексконечном цикле"""
+    user = ApiMethod(1642719191)
+    while True:
+        response = user.long_polling_timeout()
+        msg = f"Ваша работа проверена: {response['new_attempts'][0]['lesson_url']}"
+        user.send_message(msg)
+        pprint(response)
+
+
 if __name__ == '__main__':
-    a = ApiMethod(1642719191)
-    res = a.long_polling()
-    a.send_message("Привет")
-    print(res)
+    main()
