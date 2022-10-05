7cc9d967aeeec234517e08bc40a6bcf0448413a8
sergryap <rs1180@mail.ru>
2022-10-05 19:30:27 +0500

    Deleted class ApiMethod

--- api_methods.py
+++ api_methods.py
@@ -5,38 +5,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class ApiMethod:
-    """Класс для взаимодействия с api telegram и devman"""
+URL_LONG = "https://dvmn.org/api/long_polling/"
+URL_TG = "https://api.telegram.org/bot"
+TOKEN_TG = os.getenv('TOKEN_TG')
+HEADERS = {'Authorization': f'Token {os.getenv("TOKEN_DEV")}'}
 
-    URL_LONG = "https://dvmn.org/api/long_polling/"
-    URL_TG = "https://api.telegram.org/bot"
-    TOKEN_TG = os.getenv('TOKEN_TG')
-    HEADERS = {'Authorization': f'Token {os.getenv("TOKEN_DEV")}'}
 
-    def __init__(self, chat_id: int):
-        self.chat_id = chat_id
+def long_polling(timestamp=None):
+    """Функция long_polling api devman. Метод разовой проверки"""
+    url = URL_LONG
+    if timestamp:
+        url += f"?timestamp={round(timestamp, 0)}"
+    try:
+        return requests.get(URL_LONG, headers=HEADERS).json()
+    except (requests.exceptions.ReadTimeout, ConnectionError):
+        return long_polling(timestamp)
 
-    def long_polling(self, timestamp=None):
-        """Метод long_polling api devman. Метод разовой проверки"""
-        url = self.URL_LONG
-        if timestamp:
-            url += f"?timestamp={round(timestamp, 0)}"
-        try:
-            return requests.get(self.URL_LONG, headers=self.HEADERS).json()
-        except (requests.exceptions.ReadTimeout, ConnectionError):
-            return self.long_polling(timestamp)
 
-    def long_polling_timeout(self):
-        """
-        Метод получения результата проверки в режиме ожидания в цикле
-        До появления статуса о проверке работы.
-        Цикл прерывается и функция завершается, при появлении результата проверки работы
-        """
-        response = self.long_polling()
-        while response['status'] == 'timeout':
-            response = self.long_polling(timestamp=response['timestamp_to_request'])
-        return response
+def long_polling_timeout():
+    """
+    Функция получения результата проверки в режиме ожидания в цикле
+    До появления статуса о проверке работы.
+    Цикл прерывается и функция завершается, при появлении результата проверки работы
+    """
+    response = long_polling()
+    while response['status'] == 'timeout':
+        response = long_polling(timestamp=response['timestamp_to_request'])
+    return response
 
-    def send_message(self, msg: str):
-        """Отправка сообщения через api TG"""
-        requests.get(rf"{self.URL_TG}{self.TOKEN_TG}/sendmessage?chat_id={self.chat_id}&text={msg}")
+
+def send_message(chat_id: int, msg: str):
+    """Отправка сообщения через api TG"""
+    requests.get(rf"{URL_TG}{TOKEN_TG}/sendmessage?chat_id={chat_id}&text={msg}")
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from api_methods import ApiMethod
-from pprint import pprint
+from api_methods import long_polling_timeout, send_message
 
 
 def input_chat_id():
@@ -17,17 +16,15 @@ def main():
     """Функция получения результата проверок в ожидании в бексконечном цикле. Точка входа"""
     # user = ApiMethod(1642719191)
     chat_id = input_chat_id()
-    user = ApiMethod(chat_id)
-    user.send_message("Вы подключены к получению уведомлений о проверке заданий на Devmane")
+    send_message(chat_id, "Вы подключены к получению уведомлений о проверке заданий на Devmane")
     while True:
-        response = user.long_polling_timeout()
+        response = long_polling_timeout()
         head = response['new_attempts'][0]
         msg = f"Ваша работа проверена:\n" \
               f"Результат: {'РАБОТА ПРИНЯТА' if head['is_negative'] == False else 'ТРЕБУЕТСЯ ДОРАБОТКА'}\n" \
               f"lesson_title: {head['lesson_title']}\n" \
               f"lesson_url: {head['lesson_url']}"
-        user.send_message(msg)
-        pprint(response)
+        send_message(chat_id, msg)
 
 
 if __name__ == '__main__':
