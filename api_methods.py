eeaa8c3e719d05fc33b163c1c430919db6304448
sergryap <rs1180@mail.ru>
2022-10-05 19:54:17 +0500

    Environment variables to def main

--- api_methods.py
+++ api_methods.py
@@ -5,35 +5,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-URL_LONG = "https://dvmn.org/api/long_polling/"
-URL_TG = "https://api.telegram.org/bot"
-TOKEN_TG = os.getenv('TOKEN_TG')
-HEADERS = {'Authorization': f'Token {os.getenv("TOKEN_DEV")}'}
-
-
-def long_polling(timestamp=None):
+def long_polling(headers, timestamp=None):
     """Функция long_polling api devman. Метод разовой проверки"""
-    url = URL_LONG
+
+    url = "https://dvmn.org/api/long_polling/"
     if timestamp:
         url += f"?timestamp={round(timestamp, 0)}"
     try:
-        return requests.get(URL_LONG, headers=HEADERS).json()
+        return requests.get(url, headers=headers).json()
     except (requests.exceptions.ReadTimeout, ConnectionError):
         return long_polling(timestamp)
 
 
-def long_polling_timeout():
+def long_polling_timeout(headers):
     """
     Функция получения результата проверки в режиме ожидания в цикле
     До появления статуса о проверке работы.
     Цикл прерывается и функция завершается, при появлении результата проверки работы
     """
-    response = long_polling()
+    response = long_polling(headers)
     while response['status'] == 'timeout':
-        response = long_polling(timestamp=response['timestamp_to_request'])
+        response = long_polling(headers, timestamp=response['timestamp_to_request'])
     return response
 
 
-def send_message(chat_id, msg: str):
+def send_message(token, chat_id, msg: str):
     """Отправка сообщения через api TG"""
-    requests.get(rf"{URL_TG}{TOKEN_TG}/sendmessage?chat_id={chat_id}&text={msg}")
+    url = "https://api.telegram.org/bot"
+    requests.get(rf"{url}{token}/sendmessage?chat_id={chat_id}&text={msg}")
--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from api_methods import long_polling_timeout, send_message
 def main():
     """Функция получения результата проверок в ожидании в бексконечном цикле. Точка входа"""
     chat_id = os.getenv('CHAT_ID')
-    send_message(chat_id, "Вы подключены к получению уведомлений о проверке заданий на Devmane")
+    token = os.getenv('TOKEN_TG')
+    headers = {'Authorization': f'Token {os.getenv("TOKEN_DEV")}'}
+    send_message(token, chat_id, "Вы подключены к получению уведомлений о проверке заданий на Devman")
     while True:
-        response = long_polling_timeout()
+        response = long_polling_timeout(headers)
         head = response['new_attempts'][0]
         msg = f"Ваша работа проверена:\n" \
               f"Результат: {'РАБОТА ПРИНЯТА' if head['is_negative'] == False else 'ТРЕБУЕТСЯ ДОРАБОТКА'}\n" \
               f"lesson_title: {head['lesson_title']}\n" \
               f"lesson_url: {head['lesson_url']}"
-        send_message(chat_id, msg)
+        send_message(token, chat_id, msg)
 
 
 if __name__ == '__main__':
